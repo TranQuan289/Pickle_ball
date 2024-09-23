00ba997d84c8aa4854af93299753f95c6ebc64f9
TranQuan289 <anhquan28092001@gmail.com>
2024-09-23 14:03:22 +0700

    init table

--- lib/views/find_tournament/views/group_stage_view.dart
+++ lib/views/find_tournament/views/group_stage_view.dart
@@ -27,9 +27,7 @@ class GroupStageView extends ConsumerWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: IconButton(
@@ -37,11 +35,73 @@ class GroupStageView extends ConsumerWidget {
                     onPressed: () => Navigator.of(context).pop(),
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const SizedBox(
-                  height: 20,
+                const SizedBox(height: 20),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Color(0xFF1244A2), Color(0xFF062764)],
+                    ),
+                    border:
+                        Border.all(color: const Color(0xFFC6C61A), width: 0.4),
+                  ),
+                  child: Table(
+                    border: TableBorder.all(color: const Color(0xFFC6C61A)),
+                    columnWidths: const {
+                      0: FlexColumnWidth(5),
+                      1: FlexColumnWidth(1),
+                    },
+                    children: const [
+                      TableRow(
+                        children: [
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Group A',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white)))),
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('W - L',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white)))),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Row 2, Column 1',
+                                      style: TextStyle(color: Colors.white)))),
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Row 2, Column 2',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white)))),
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Row 3, Column 1',
+                                      style: TextStyle(color: Colors.white)))),
+                          TableCell(
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Row 3, Column 2',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white)))),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
--- lib/views/find_tournament/widgets/item_competition_format_widget.dart
+++ lib/views/find_tournament/widgets/item_competition_format_widget.dart
@@ -28,7 +28,7 @@ class ItemCompetitionFormatWidget extends StatelessWidget {
         } else if (tournament.tournamentType == 'GroupStage') {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => GroupStageView()),
+            MaterialPageRoute(builder: (context) => const GroupStageView()),
           );
         }
       },
